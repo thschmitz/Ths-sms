d6a711c4f5c69dec712878bca779511404abb24a
thschmitz <thomas.henrique.schmitz@gmail.com>
2022-09-10 16:30:54 -0300

    DeploymentDone

--- server/index.js
+++ server/index.js
@@ -7,6 +7,11 @@ const port = process.env.PORT || 5000;
 
 require("dotenv").config();
 
+const accountSid = process.env.TWILIO_ACCOUNT_SID;
+const authToken = process.env.TWILIO_AUTH_TOKEN;
+const messagingServiceSid = process.env.TWILIO_MESSAGING_SERVICE_ID;
+const twilioClient = require("twilio")(accountSid, authToken);
+
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded());
@@ -14,6 +19,27 @@ app.use("/auth", authRoutes)
 
 app.get("/", (req, res) => {
     res.send("Hello World")
+})
+
+app.post("/", (req, res) => {
+    const {message, user: sender, type, members} = req.body;
+    console.log(message)
+    if(type==="message.new") {
+        members.filter((member) => member.user.id !== sender.id).forEach(({user}) => {
+            if(!user.online) {
+                twilioClient.messages.create({
+                    body: `You have a new message ${message.user.fullName} - ${message-text}`,
+                    messagingServiceSid: messagingServiceSid,
+                    to: user.phoneNumber
+                }).then(() => {
+                    console.log("Message Sent!");
+                }).catch((err) => console.log(err));
+            }
+        })
+        return res.status(200).send("Message Sent!");
+    }
+
+    return res.status(200).send("Not a new message request");
 })
 
 app.listen(port, () => console.log(`Server running on port ${port}`))
